34c0b06b1193d041f4c2f74aa4ef78afb74b4d3a
Ubuntu <ubuntu@ip-172-31-19-116.ec2.internal>
2022-01-21 10:22:58 +0000

    configured nginx to handle static content

--- src/node/server.js
+++ src/node/server.js
@@ -1,18 +1,20 @@
-const port = 80;
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
 const router = express.Router();
 
+const port = 3000;
 const html_path = __dirname + '/../html';
-const img_path  = __dirname + '/../img';
+// configured nginx to handle static content
+//const img_path  = __dirname + '/../img';
 
 router.get('/', function (req, res) {
 	res.sendFile(path.join(html_path + '/index.html'));
 });
 
 app.use('/', router);
-app.use('/img', express.static(img_path));
+//app.use('/img', express.static(img_path));
 app.listen(process.env.port || port);
 
 console.log('Running at Port ' + port);
