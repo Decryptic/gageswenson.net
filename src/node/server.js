f20b311850c69868ab71385f32c71cbd3c403baf
Ubuntu <ubuntu@ip-172-31-19-116.ec2.internal>
2022-01-23 12:00:07 +0000

    added navigation bar and prototype scripts

--- src/node/server.js
+++ src/node/server.js
@@ -2,16 +2,42 @@ const express   = require('express');
 const sprightly = require('sprightly');
 const path      = require('path');
 
-const port      = 3000;
-const html_path = path.join(__dirname, '../html');
+const port    = 3000;
+const html_d  = path.join(__dirname, '../html');
 
 const app = express();
 
 app.get('/', function (_, res) {
-	res.render('index.spy', { title: "Gage's Website Yo" });
+	res.render('index.spy', {
+        title:      "Gage's Website",
+        favicon:    'index_favicon.ico',
+        script:     'index.js',
+        stylesheet: 'index.css',
+        home:  'active',
+    });
+});
+
+app.get('/blog', function (_, res) {
+    res.render('blog.spy', {
+        title:      "Gage's Blog",
+        favicon:    'blog_favicon.ico',
+        script:     'blog.js',
+        stylesheet: 'blog.css',
+        blog:  'active',
+    });
+});
+
+app.get('/contact', function (_, res) {
+    res.render('contact.spy', {
+        title:      "Contact Gage",
+        favicon:    'contact_favicon.ico',
+        script:     'contact.js',
+        stylesheet: 'contact.css',
+        contact:  'active',
+    });
 });
 
 app.engine('spy', sprightly);
-app.set('views', html_path);
+app.set('views', html_d);
 app.set('view engine', 'spy');
-app.listen(port, console.log('running at port' + port));
+app.listen(port, console.log('running at port ' + port));
